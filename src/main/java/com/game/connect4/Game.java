86b1a763e38e1e8d4d458584f1a6c02d054a0376
Chris Barran <chris.d.barran@gmail.com>
2019-12-13 19:49:09 +0000

    Tests initialisation of game

--- src/main/java/com/game/connect4/Game.java
+++ src/main/java/com/game/connect4/Game.java
@@ -9,9 +9,18 @@ import lombok.experimental.Accessors;
 @Accessors(fluent = true) @Getter
 public class Game {
 
-    
+    private static final int ROWS = 6;
+
+    private static final int COLUMNS = 7;
+
+    private static final int PLAYER1 = 1;
+
     private final @NonNull String player1;
 
     private final @NonNull String player2;
+
+    private int who = PLAYER1;
+
+    private int[][] board = new int[ROWS][COLUMNS];
 
 }
--- src/test/java/com/game/connect4/GameTest.java
+++ src/test/java/com/game/connect4/GameTest.java
@@ -2,20 +2,48 @@ package com.game.connect4;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
 
+    private static final String PLAYER1 = "player1";
+
+    private static final String PLAYER2 = "player2";
+
+    private Game game;
+
+    @BeforeEach
+    void setup() {
+        game = new Game(PLAYER1, PLAYER2);
+    }
 
     @Test
     void newGameWithPlayers() {
-        String player1 = "player1";
-        String player2 = "player2";
-        Game game = new Game(player1, player2);
         assertNotNull(game, "Game should initialise with players");
-        assertEquals(player1, game.player1());
-        assertEquals(player2, game.player2());
+        assertEquals(PLAYER1, game.player1());
+        assertEquals(PLAYER2, game.player2());
     }
+
+    @Test
+    void newGameStartsWithPlayer1() {
+        assertEquals(1,game.who(), "Game should start with player1");
+    }
+
+    @Test
+    void newGameStartsWithEmptyBoard() {
+        // Arrays won't match need to use Arrays.equals()
+        // However this doesn't work for arrays of arrays
+        // So use deepEquals
+        int[][] emptyBoard = new int[6][7];
+        assertTrue(Arrays.deepEquals(emptyBoard, game.board()));
+    }
+
+
+
 
 }
