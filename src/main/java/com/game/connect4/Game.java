326759ae5d12de8c9b560e360ed685077e5b98ad
Chris Barran <chris.d.barran@gmail.com>
2019-12-13 17:32:44 +0000

    Initialise game with two players

--- src/main/java/com/game/connect4/Game.java
+++ src/main/java/com/game/connect4/Game.java
@@ -2,7 +2,25 @@ package com.game.connect4;
 
 public class Game {
 
+    String player1;
+
+    String player2;
+
     public Game() {
         
+    }
+
+    public Game(String player1, String player2)
+    {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public String getPlayer1(){
+        return this.player1;
+    }
+
+    public String getPlayer2(){
+        return this.player2;
     }
 }
--- src/test/java/com/game/connect4/GameTest.java
+++ src/test/java/com/game/connect4/GameTest.java
@@ -1,5 +1,6 @@
 package com.game.connect4;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,18 @@ import org.junit.jupiter.api.Test;
 public class GameTest {
 
     @Test
-    void loadGame() {
+    void newGame() {
         assertNotNull(new Game(),"Game is null");
+    }
+
+    @Test
+    void newGameWithPlayers() {
+        String player1 = "player1";
+        String player2 = "player2";
+        Game game = new Game(player1, player2);
+        assertNotNull(game, "Game should initialise with players");
+        assertEquals(player1, game.getPlayer1());
+        assertEquals(player2, game.getPlayer2());
     }
 
 }
